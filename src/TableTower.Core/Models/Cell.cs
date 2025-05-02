baee84b21b3bc064a46c829e37082128e9767d9c
Mahammad <dev.ahmadov.mahammad@gmail.com>
2025-05-03 01:46:10 +0400

    add core models that we will use them later for building tables

---
+++ src/TableTower.Core/Models/Cell.cs
@@ -0,0 +1,31 @@
+﻿using TableTower.Core.Enums;
+
+namespace TableTower.Core.Models;
+public class Cell : IEquatable<Cell>
+{
+    public object? Value { get; }
+    public HorizontalAlignment HorizontalAlignment { get; }
+    public ConsoleColor? Foreground { get; }
+
+    public Cell(object? value, HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left, ConsoleColor? foreground = null)
+    {
+        Value = value;
+        HorizontalAlignment = horizontalAlignment;
+        Foreground = foreground;
+    }
+
+    public bool Equals(Cell? other)
+    {
+        if (other == null) return false;
+        return Equals(Value, other.Value) &&
+               HorizontalAlignment == other.HorizontalAlignment &&
+               Foreground == other.Foreground;
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as Cell);
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Value, HorizontalAlignment, Foreground);
+    }
+}
---
+++ src/TableTower.Core/Models/Table.cs
@@ -0,0 +1,47 @@
+﻿namespace TableTower.Core.Models;
+public class Table : IEquatable<Table>
+{
+    public string? Title { get; }
+    public IReadOnlyList<Column> Columns { get; }
+    public IReadOnlyList<Row> Rows { get; }
+    public bool ShowRowLines { get; }
+
+    public Table(string? title, IReadOnlyList<Column> columns, IReadOnlyList<Row> rows, bool showRowLines)
+    {
+        Title = title;
+        Columns = columns;
+        Rows = rows;
+        ShowRowLines = showRowLines;
+    }
+
+    public bool Equals(Table? other)
+    {
+        if (other == null) return false;
+        return Title == other.Title &&
+               Columns.SequenceEqual(other.Columns) &&
+               Rows.SequenceEqual(other.Rows) &&
+               ShowRowLines == other.ShowRowLines;
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as Table);
+
+    public override int GetHashCode()
+    {
+        HashCode hashCode = new HashCode();
+
+        foreach (Column column in Columns)
+        {
+            hashCode.Add(column.GetHashCode());
+        }
+
+        foreach (Row row in Rows)
+        {
+            hashCode.Add(row.GetHashCode());
+        }
+
+        hashCode.Add(Title);
+        hashCode.Add(ShowRowLines);
+
+        return hashCode.ToHashCode();
+    }
+}
